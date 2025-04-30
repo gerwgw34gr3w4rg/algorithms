499337643065a3fc03f987ad16f0286edb0df3e5
gerwgw34gr3w4rg <3g3r8g94eotgrtg4thethe@mail.ru>
2025-04-30 22:46:41 +0300

    Delete fiels invalid names

--- ascii_to_unit64/error.c
+++
@@ -1,8 +0,0 @@
-#include <stdlib.h>
-#include <stdio.h>
-
-
-void null_pointer_error(){
-    fprintf(stderr, "NULL pointer error: %s: %s(): Line:%d\n", __FILE__, __FUNCTION__, __LINE__ -1);
-    abort();
-}
